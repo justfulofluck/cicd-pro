4f2a70b2cb6e2be708c7c590ce6bf574edaccf66
Bhavan <115586675+justfulofluck@users.noreply.github.com>
2023-05-15 16:44:24 +0530

    minor changes

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-# def to_upper(name):
-#     return name.upper()
-
-# def say_hello(name):
-#     print(f'hello, {name}')
-
-# if __name__ == '__main__':
-#     name = 'bhavan'
-#     say_hello(name)
-#     up = to_upper(name)
-#     print(up) 
-
 def to_upper(name):
     return name.upper()
 
@@ -21,7 +9,7 @@ def say_hello(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    name = 'TrainWithShubham'
+    name = 'Bhavan'
     say_hello(name)
     up = to_upper(name)
     print(up)
--- test.py
+++ test.py
@@ -1,37 +1,12 @@
-# import unittest
-# from io import StringIO
-# from unittest.mock import patch
-# from main import to_upper, say_hello
-
-
-# class MyTestCase(unittest.TestCase):
-#     def test_to_upper(self):
-#         name = 'bhavan'
-#         uupper_name = to_upper(name)
-#         self.assertEqual(uupper_name, 'BHAVAN')
-
-#     def test_say_hello(self):
-#         name = 'bhavan'
-#         expected_output = 'Hello, bhavan\n'
-
-#         with patch('sys.stdout', new=StringIO()) as fake_output:
-#             say_hello(name)
-#             self.assertEqual(fake_output.getvalue(), expected_output)
-
-
-# if __name__ == '__main__':
-#     unittest.main()
-
-
 import unittest
 from main import to_upper
 
 
 class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
-        name = "Shubham"
+        name = "Bhavan"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "SHUBHAM")
+        self.assertEqual(upper_name, "BHAVAN")
 
 
 if __name__ == '__main__':
