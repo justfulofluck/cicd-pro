222a6d09e6ed3b5e368631d29a14411d429a195f
Bhavan <115586675+justfulofluck@users.noreply.github.com>
2023-05-15 16:29:41 +0530

    change testcases

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
 import unittest
-from main import to_upper
+from io import StringIO
+from unittest.mock import patch
+from main import to_upper, say_hello
+
 
 class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
         name = 'bhavan'
         uupper_name = to_upper(name)
-        self.assertEqual(uupper_name, 'bhavan')
+        self.assertEqual(uupper_name, 'BHAVAN')
+
+    def test_say_hello(self):
+        name = 'bhavan'
+        expected_output = 'Hello, bhavan\n'
+
+        with patch('sys.stdout', new=StringIO()) as fake_output:
+            say_hello(name)
+            self.assertEqual(fake_output.getvalue(), expected_output)
+
 
 if __name__ == '__main__':
     unittest.main()
-    
